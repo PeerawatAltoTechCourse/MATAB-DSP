9d01c48f9cdac09e59003d902714071f86eb2b01
PeerawatAltoTechCourse <peerawat.y@ku.th>
2023-06-28 01:04:40 +0700

    u1update

--- discrete_unit_impulse.m
+++ discrete_unit_impulse.m
@@ -1,7 +1,7 @@
 % Hello , Let's start learning DSP by MATLAB
 
-n=-4:4; 
-delta_n=[zeros(1,4) 1 zeros(1,4)] ; 
+n=-5:5; 
+delta_n=[zeros(1,5) 1 zeros(1,5)] ; 
 stem(n,delta_n);
 xlabel('Time sample');
 ylabel('Amplitude');
